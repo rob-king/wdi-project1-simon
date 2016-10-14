de63594111e85a99f2bb89083c51061260a68e76
robert <robert.k25@gmail.com>
2016-10-14 11:53:18 -0400

    adds basic shuffling

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,9 +1,22 @@
+const colors = ['red', 'blue', 'yellow', 'green'];
 
 $(document).ready(function() {
   console.log("jquery working");
 
-
   $("body").on("keydown", getKeyCode);
 
-
+  base = sequence(3);
 });
+
+function sequence(n) {
+  var result = [];
+  for (var i = 0; i < n; i++) {
+    elem = colors[Math.floor(Math.random()*colors.length)];
+    result.push(elem);
+  }
+  return _.shuffle(result);
+}
+
+function generateRound(base_seq, extend_by) {
+  return _.concat(base_seq, sequence(extend_by));
+}
